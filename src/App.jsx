cb1bb3c4ece463fe7ca86eb9f892d6f59a3d6b53
Adriano Rotondo <adriano.rotondo.dev@gmail.com>
2025-07-21 12:33:20 +0200

    add base project structure, render films array, add base form imput to render new film genre in  list

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+
+import filmList from "./data/filmList";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [newFilm, setNewFilm] = useState("");
+  const [films, setFilms] = useState(filmList);
 
+  //*crea un inserimento per il genere del film
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    // è un oggetto, non una stringa.
+    setFilms([{ genre: newFilm }, ...films]); // inserisco il nuovo genere del film ed evito di sovrascrivere l'intero array precedente
+  }
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div className="container flex">
+      <h2>This is your Film List</h2>
+      <ul className="list flex">
+        {films.map((film, index) => {
+          return (
+            <li key={index} className="flex">
+              <span>{film.title}</span>
+              <span>{film.genre}</span>
+            </li>
+          );
+        })}
+      </ul>
+      {/* al momento inserisco unicamente il genere per verificare il filtraggio */}
+      {/* inserire un secondo imput per il titolo o farlo in solo imput? */}
+      <form onSubmit={handleSubmit} className="form flex">
+        <input
+          className="input"
+          placeholder="Insert New Film"
+          type="text"
+          value={newFilm}
+          onChange={(e) => setNewFilm(e.target.value)}
+        />
+        <button type="submit" className="save btn flex">
+          Save the New Film
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      </form>
+    </div>
+  );
 }
 
-export default App
+export default App;
